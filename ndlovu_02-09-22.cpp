f0b2a5e69db73c8ef4d822478230aff6acffe271
Simba-JSU <j00953204@students.jsums.edu>
2022-02-09 23:14:27 -0600

    add the cpp file

---
+++ ndlovu_02-09-22.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	string secrets[5][5] = { {"car", "cheque($1000)", "debt($1000)", "donation($1000)","insuarance(health/1y)"},
+							{"trip(Orlando)","girlfriend","job(software developer)","house","scholarship"},
+							{"loan($2000)","factory","company","nothing","lottery($25000)"},
+							{"beer", "host a party", "good fortune","bad luck","bankrupt"},
+							{"convict","dumped","loser","genuis","chill"} };
+	
+	return 0;
+}
+	
+
+	
+
